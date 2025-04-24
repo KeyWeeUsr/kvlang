567c76ae28eefa99445a3f69cebdb199a1da1116
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2025-04-24 23:04:48 +0200

    [ADD] setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from typing import Mapping, List, Union, Collection, Dict
+
+KWARGS: Mapping[str, str] | Mapping[str, Collection[str]] = {
+    "name": "kvlang",
+    "install_requires": ["lark>=1.2.2"],
+    "extras_require": {"dev": [
+        "pycodestyle", "pylint", "mypy",
+        "types-setuptools"
+    ]}
+}
+
+
+if __name__ == "__main__":
+    setup(**KWARGS)
