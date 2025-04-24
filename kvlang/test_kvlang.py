c4c5b3cd61eeb12d5d6e4baf0467f07f32c53e90
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2025-04-25 00:42:43 +0200

    [ADD] `#:kv` directive

--- kvlang/test_kvlang.py
+++ kvlang/test_kvlang.py
@@ -5,7 +5,7 @@ util._MAX_LENGTH = 999999  # type: ignore
 
 
 class TestGrammar(TestCase):
-    def test_version_lone(self):
+    def test_kivy_version_lone(self):
         from kvlang import parse
         text = "#:kivy 1.0"
         out = parse(text)
@@ -18,7 +18,7 @@ class TestGrammar(TestCase):
             ]
         ))
 
-    def test_version_newlined(self):
+    def test_kivy_version_newlined(self):
         from kvlang import parse
         text = "#:kivy 1.0\n"
         out = parse(text)
@@ -27,6 +27,49 @@ class TestGrammar(TestCase):
                 Tree(Token('RULE', 'header'), [
                     Token('KIVY_VERSION', text.rstrip("\n")),
                     Token("NEWLINE", "\n")
+                ]),
+                Tree(Token('RULE', 'content'), [])
+            ]
+        ))
+
+    def test_kv_version_lone(self):
+        from kvlang import parse
+        text = "#:kv 1.0"
+        out = parse(text)
+        self.assertEqual(out, Tree(
+            Token('RULE', 'start'), [
+                Tree(Token('RULE', 'header'), [
+                    Token('KV_VERSION', text)
+                ]),
+                Tree(Token('RULE', 'content'), [])
+            ]
+        ))
+
+    def test_kv_version_newlined(self):
+        from kvlang import parse
+        text = "#:kv 1.0\n"
+        out = parse(text)
+        self.assertEqual(out, Tree(
+            Token('RULE', 'start'), [
+                Tree(Token('RULE', 'header'), [
+                    Token('KV_VERSION', text.rstrip("\n")),
+                    Token("NEWLINE", "\n")
+                ]),
+                Tree(Token('RULE', 'content'), [])
+            ]
+        ))
+
+    def test_kivy_kv_version_newlined(self):
+        from kvlang import parse
+        kv = "#:kv 1.0"
+        kivy = "#:kivy 1.0"
+        out = parse("\n".join([kv, kivy]))
+        self.assertEqual(out, Tree(
+            Token('RULE', 'start'), [
+                Tree(Token('RULE', 'header'), [
+                    Token('KV_VERSION', kv),
+                    Token("NEWLINE", "\n"),
+                    Token('KIVY_VERSION', kivy)
                 ]),
                 Tree(Token('RULE', 'content'), [])
             ]
