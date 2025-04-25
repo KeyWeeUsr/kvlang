3bd2858ce6c387a7982365a47aafd790bcef0571
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2025-04-26 01:24:42 +0200

    [ADD] Test for `canvas` as widgets

--- kvlang/test_examples.py
+++ kvlang/test_examples.py
@@ -16,6 +16,18 @@ GETTING_STARTED = """
         rows: 2 # this how you set each property of your widget/layout
 """
 
+# 6e23344/doc/sources/images/gs-drawing.png
+GS_DRAWING = """
+#kivy 1.0
+<MyWidget>
+    canvas:
+        Color:
+            rgba: .5,.5,.5,.5
+        Ellipse:
+            size: self.size
+            pos: self.pos
+"""
+
 
 class TestExamples(TestCase):
     def test_getting_started(self):
@@ -32,6 +44,43 @@ class TestExamples(TestCase):
                     Tree(Token('RULE', 'widget_property'), [
                         Token('PROPERTY_NAME', 'rows'),
                         Token('PROPERTY_VALUE', '2')
+                    ])
+                ])
+            ])
+        ]))
+
+    def test_getting_started_drawing(self):
+        from kvlang import parse
+        self.assertEqual(parse(GS_DRAWING), Tree(Token('RULE', 'start'), [
+            Tree(Token('RULE', 'widget_rule_tree'), [
+                Tree(Token('RULE', 'widget_rule'), [
+                    Token('WIDGET_RULE', 'MyWidget')
+                ]),
+                Tree(Token('RULE', 'widget_tree'), [
+                    Tree(Token('RULE', 'widget'), [
+                        Token('WIDGET', 'canvas')
+                    ]),
+                    Tree(Token('RULE', 'widget_tree'), [
+                        Tree(Token('RULE', 'widget'), [
+                            Token('WIDGET', 'Color')
+                        ]),
+                        Tree(Token('RULE', 'widget_property'), [
+                            Token('PROPERTY_NAME', 'rgba'),
+                            Token('PROPERTY_VALUE', '.5,.5,.5,.5')
+                        ])
+                    ]),
+                    Tree(Token('RULE', 'widget_tree'), [
+                        Tree(Token('RULE', 'widget'), [
+                            Token('WIDGET', 'Ellipse')
+                        ]),
+                        Tree(Token('RULE', 'widget_property'), [
+                            Token('PROPERTY_NAME', 'size'),
+                            Token('PROPERTY_VALUE', 'self.size')
+                        ]),
+                        Tree(Token('RULE', 'widget_property'), [
+                            Token('PROPERTY_NAME', 'pos'),
+                            Token('PROPERTY_VALUE', 'self.pos')
+                        ])
                     ])
                 ])
             ])
