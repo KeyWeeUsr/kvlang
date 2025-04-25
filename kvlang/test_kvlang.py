69a610d73cc80e87f64d4a23954e9753a4a811f0
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2025-04-25 23:35:41 +0200

    [ADD] Test for widget rule trees

--- kvlang/test_kvlang.py
+++ kvlang/test_kvlang.py
@@ -161,6 +161,28 @@ class TestGrammar(TestCase):
                 ])
             ]))
 
+    def test_root_widget_tree(self):
+        from kvlang import parse
+        for prefix, comment in (("", "text"), (" ", " text"), ("    ", "    text")):
+            text = "<MyWidget>:\n Widget:\n  Widget:"
+            self.assertEqual(parse(text), Tree(Token("RULE", "start"), [
+                Tree(Token("RULE", "widget_rule_tree"), [
+                    Tree(Token("RULE", "widget_rule"), [
+                        Token("WIDGET_RULE", "<MyWidget>")
+                    ]),
+                    Tree(Token("RULE", "widget_tree"), [
+                        Tree(Token("RULE", "widget"), [
+                            Token("WIDGET", "Widget")
+                        ]),
+                        Tree(Token("RULE", "widget_tree"), [
+                            Tree(Token("RULE", "widget"), [
+                                Token("WIDGET", "Widget")
+                            ])
+                        ])
+                    ])
+                ])
+            ]))
+
 
 class TestQuirks(TestCase):
     def test_number_as_variable(self):
