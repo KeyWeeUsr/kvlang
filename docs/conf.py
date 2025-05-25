2bbf881da615a9ed69678b00381d320fea4658f6
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2025-05-25 11:54:37 +0200

    [FIX] Docs generating

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
-# import os, sys
-# sys.path.insert(0, os.path.abspath('..'))
+import os, sys
+sys.path.insert(0, os.path.abspath('..'))
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
